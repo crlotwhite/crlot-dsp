c26fbf1bd23ef978c5123996559697485ddbc0e5
crlotwhite <crlotwhite@gmail.com>
2025-08-28 22:47:01 +0900

    feat: Add dr_wav library and sample code

--- main/main.cc
+++ main/main.cc
@@ -1,12 +1,23 @@
 #include <fmt/base.h>
 #include <fmt/chrono.h>
 
+#define DR_WAV_IMPLEMENTATION
+#include "dr_libs/dr_wav.h"
+
 int main() {
   fmt::print("Hello, world!\n");
 
   auto now = std::chrono::system_clock::now();
   fmt::print("Date and time: {}\n", now);
   fmt::print("Time: {:%H:%M}\n", now);
+
+  drwav wav;
+  if (!drwav_init_file(&wav, "assets/oboe.wav", nullptr)) {
+    fmt::print("Failed to open WAV file.\n");
+    return -1;
+  } else {
+    fmt::print("WAV file opened successfully.\n");
+  }
 
   return 0;
 }
